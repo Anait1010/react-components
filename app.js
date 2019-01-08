feafe89e6cce877e96edd6926a3b915a0399d0fa
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2019-01-07 22:46:06 -0800

    Complete adding 2 nested item components

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// TODO
+var GroceryList = () => (
+ <ul> 
+   <li><Cheese/></li>
+   <li><Strawberries/></li>
+ </ul>
+);
+
+var Cheese = () => (
+  <div>Cheese</div>
+);
+var Strawberries = () => (
+  <div>Strawberries </div>
+);
+
+
+ReactDOM.render(<GroceryList/>, document.getElementById('app'));
