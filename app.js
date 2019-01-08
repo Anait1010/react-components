ee9dbea7fb533a8aedb274a683f8d8d2afcd4a12
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2019-01-08 08:54:47 -0800

    Complete Bare Minimum Requirements

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
 var GroceryList = (props) => (
- <ul> 
-   <li>{props.items[0]}</li>
-   <li>{props.items[1]}</li>
-   <li>{props.items[2]}</li>
- </ul>
+  <ul>
+  {props.items.map(item =>
+    <GroceryListItem item={item} />
+  )}
+</ul> 
 );
 
-var GroceryListItem = () => (
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    }
+  }
+  // OnListItemCLick() {
+  //   this.setState({
+  //     done: !this.state.done
+  //   });
+  // }
+
+  OnListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  OnListItemLeave() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      // textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.done ? 'bold' : 'none'
+    };
+      
+    return (
+      <li style={style} onMouseLeave={this.OnListItemLeave.bind(this)} onMouseEnter={this.OnListItemHover.bind(this)}>{this.props.item}</li>
+    )
+  };
+};
+
+var App = () => (
   <div>
     <h2>My Grocery List</h2>
      <GroceryList items={['Cheese', 'Strawberries', 'Avocados']}/>
-  </div>
-  
+  </div> 
 );
 
+ReactDOM.render(<App/>, document.getElementById('app'));
 
-ReactDOM.render(<GroceryListItem/>, document.getElementById('app'));
